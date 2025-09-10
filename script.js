e1b4d10740c0fc3ab5d01870b9f3fdcd156b1a3a
John Ashley Alday <johnashley22.github@gmail.com>
2025-09-10 19:20:31 +0800

    my first commit

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function calculate(){
             result = num1 + num2;
             break;
         case "-":
-            result = num1 = num2;
+            result = num1 - num2;
             break;
         case "*":
             result = num1 * num2;
